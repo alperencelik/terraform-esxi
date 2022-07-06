0c3614c3b2a02e92cbc8aa06f566a688f13aadbb
root <root@centos-template.vm.local>
2022-07-06 16:17:15 +0300

    update output file

--- output.tf
+++ output.tf
@@ -1,36 +1,36 @@
 output "vm_ip_address_small_0" {
-  value = "[${esxi_guest.small[0].ip_address}]"
+  value = "[${esxi_guest.small[0].guest_name}=${esxi_guest.small[0].ip_address}]"
 }
 
 output "vm_ip_address_small_1" {
-  value = "[${esxi_guest.small[1].ip_address}]"
+  value = "[${esxi_guest.small[1].guest_name}=${esxi_guest.small[1].ip_address}]"
 }
 
 output "vm_ip_address_small_2" {
-  value = "[${esxi_guest.small[2].ip_address}]"
+  value = "[${esxi_guest.small[2].guest_name}=${esxi_guest.small[2].ip_address}]"
 }
 
 output "vm_ip_address_medium_0" {
-  value = "[${esxi_guest.medium[0].ip_address}]"
+  value = "[${esxi_guest.medium[0].guest_name}=${esxi_guest.medium[0].ip_address}]"
 }
 
 output "vm_ip_address_medium_1" {
-  value = "[${esxi_guest.medium[1].ip_address}]"
+  value = "[${esxi_guest.medium[1].guest_name}=${esxi_guest.medium[1].ip_address}]"
 }
 
 output "vm_ip_address_medium_2" {
-  value = "[${esxi_guest.medium[2].ip_address}]"
+  value = "[${esxi_guest.medium[2].guest_name}=${esxi_guest.medium[2].ip_address}]"
 }
 
 output "vm_ip_address_medium_3" {
-  value = "[${esxi_guest.medium[3].ip_address}]"
+  value = "[${esxi_guest.medium[3].guest_name}=${esxi_guest.medium[3].ip_address}]"
 }
 
 output "vm_ip_address_medium_4" {
-  value = "[${esxi_guest.medium[4].ip_address}]"
+  value = "[${esxi_guest.medium[4].guest_name}=${esxi_guest.medium[4].ip_address}]"
 }
 
 output "vm_ip_address_medium_5" {
-  value = "[${esxi_guest.medium[5].ip_address}]"
+  value = "[${esxi_guest.medium[5].guest_name}=${esxi_guest.medium[5].ip_address}]"
 }
 
